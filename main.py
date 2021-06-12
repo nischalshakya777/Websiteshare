ce324e0a225e352503e5981ba487e461d029a01c
nischalshakya777 <69291726+nischalshakya777@users.noreply.github.com>
2021-06-12 21:03:02 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
 from bs4 import BeautifulSoup
+
 import requests
-import lxml
+
 import csv
 
-url = "https://merolagani.com/LatestMarket.aspx"
-
-con = requests.get(url)
-soup = BeautifulSoup(con.text, 'lxml')
-table = soup.find('table', class_="table table-hover live-trading sortable")
-headers = [i.text for i in table.find_all('th')]
-data = [j for j in table.find_all('tr', {"class": ["decrease-row", "increase-row", "no-change-row"]})]
-result = [{headers[index]: cell.text for index, cell in enumerate(row.find_all("td"))} for row in data]
-print(result)
 csv_file = open('nepse.csv', 'w')
-
 csv_writer = csv.writer(csv_file)
+csv_writer.writerow('s')
 
-csv_writer.writerow(result)
+for i in range(1, 12):
+    url = ["http://www.nepalstock.com/main/todays_price/index/1/",  # for Nepse market data
+           "http://www.nepalstock.com/main/todays_price/index/2/",
+           "http://www.nepalstock.com/main/todays_price/index/3/",
+           "http://www.nepalstock.com/main/todays_price/index/4/",
+           "http://www.nepalstock.com/main/todays_price/index/5/",
+           "http://www.nepalstock.com/main/todays_price/index/6/",
+           "http://www.nepalstock.com/main/todays_price/index/7/",
+           "http://www.nepalstock.com/main/todays_price/index/8/",
+           "http://www.nepalstock.com/main/todays_price/index/9/",
+           "http://www.nepalstock.com/main/todays_price/index/10/",
+           "http://www.nepalstock.com/main/todays_price/index/11/"]
+    print(url[i - 1])
+    u = url[i - 1]
+    source = requests.get(u)
+    soup = BeautifulSoup(source.text, 'lxml')
+    contain = soup.find('div', class_='container')
+    data = contain.find_next('tr', class_='unique').find_all_next('td')
+    csv_file = open('nepse.csv', 'a')
+    csv_writer = csv.writer(csv_file)
+    for d in data:
+        print(d.text)
+        csv_writer.writerow([d.text])
+
+url = "http://www.nepalstock.com/"
+source = requests.get(url)
+soup = BeautifulSoup(source.text, 'lxml')
+data = soup.find_all('table', class_='table table-hover table-condensed')
+csv_file = open('nepsemainpage.csv', 'w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow('s')
+
+for d in data:
+    raw = d.find_all('td')
+    for r in raw:
+        l = r.find_all('img')
+        csv_file = open('nepsemainpage.csv', 'a')
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow([r.text])
+        print(r.text)
