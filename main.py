3d3c9e65dbc2dc9a834c8fa8e19043f364501dcf
nischalshakya777 <69291726+nischalshakya777@users.noreply.github.com>
2021-06-28 17:33:24 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 import lxml
+import pandas as pd
 from csv import writer
 from csv import DictWriter
+
 import os
 
 # Site URL
 url = "https://www.sharesansar.com/market"
 
-# make a Get request to fentch the raw HTML data
+# make a Get request to fetch the raw HTML data
 con = requests.get(url)
 soup = BeautifulSoup(con.text, 'lxml')  # code of the page
 
@@ -19,18 +21,17 @@ with open('index.csv', 'w') as f_object:
     writer_object = writer(f_object)
     writer_object.writerow(headers)
     f_object.close()
-data = [j.text for j in table.find_all('td')]  # data inside the tr tag
+data = [j.text for j in table.find_all('td')]
+data = [item.replace(',', '') for item in data if str(item)]
 i = 0
 while i < 13:
-    dict = {'Index': data[i], 'Close': data[i + 1], 'Point Change': data[i + 2],
-            '% Change': data[i + 3]}
+    dict = {headers[0]: data[i], headers[1]: data[i + 1], headers[2]: data[i + 2],
+            headers[3]: data[i + 3]}
     with open('index.csv', 'a') as f_object:
         dictwriter_object = DictWriter(f_object, fieldnames=headers)
         dictwriter_object.writerow(dict)
         f_object.close()
     i += 4
-
-
 
 os.system('python ../pythonProject3/Nepse.py')
 table1 = soup.find_all('th')[25:-21]
@@ -41,18 +42,17 @@ with open('Sub-Indices.csv', 'w') as f_object:
     f_object.close()
 table2 = soup.find_all('td')[28:-122]
 data1 = [j.text for j in table2]
+data1 = [item.strip() for item in data1 if str(item)]
+data1 = [item.replace(',', '') for item in data1 if str(item)]
+
 i = 0
 while i < 65:
-    dict = {'Sub-Indices': data1[i], 'Turnover': data1[i + 1], 'Close': data1[i + 2], 'Point': data1[i+3],
-            '% Change': data1[1+4]}
+    dict = {head[0]: data1[i], head[1]: data1[i + 1], head[2]: data1[i + 2], head[3]: data1[i + 3],
+            head[4]: data1[i + 4]}
     with open('Sub-Indices.csv', 'a') as f_object:
         dictwriter_object = DictWriter(f_object, fieldnames=head)
         dictwriter_object.writerow(dict)
         f_object.close()
     i += 5
-print(headers)
-print(data)
-print(head)
-print(data1)
-#change location if file in different location
+
 os.system('python ../pythonProject3/Nepse.py')
